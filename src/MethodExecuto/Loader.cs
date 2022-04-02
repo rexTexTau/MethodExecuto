307216714781f9287f6f7564624e7281571bc19c
George Churochkin <rextextau@gmail.com>
2022-04-02 23:06:22 +0300

    initial code commit (some StyleCop warnings remain)

---
+++ src/MethodExecuto/Loader.cs
@@ -0,0 +1,156 @@
+﻿// Copyright (c) 2022 George Churochkin. All rights reserved.
+// Licensed under the MIT License (https://opensource.org/licenses/MIT)
+// Full license text: see LICENSE file in the root folder of this repository
+
+namespace rextextau.MethodExecuto
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Utility class to perform reflection ops.
+    /// </summary>
+    internal static class Loader
+    {
+        public static string GetAssemblyDir(Assembly assembly)
+        {
+            string codeBase = assembly.Location;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+
+        public static Assembly LoadAssembly(string assemblyFilePath)
+        {
+            Assembly a = null;
+            try
+            {
+                a = Assembly.LoadFile(assemblyFilePath);
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"Error loading {Path.GetFileName(assemblyFilePath)}");
+            }
+
+            return a;
+        }
+
+        private static Type[] LoadTypes(Assembly a)
+        {
+            Type[] types;
+            try
+            {
+                types = a.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                types = e.Types;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return types.Where(t => t != null &&
+                    Attribute.GetCustomAttribute(t, typeof(CompilerGeneratedAttribute)) == null &&
+                    !t.Attributes.HasFlag(TypeAttributes.NestedPrivate))
+                .ToArray();
+        }
+
+        public static Type[] LoadTypes(string assemblyFilePath)
+        {
+            Assembly a = Assembly.LoadFile(assemblyFilePath);
+            if (a == null)
+            {
+                throw new Exception($"Error loading {Path.GetFileName(assemblyFilePath)}");
+            }
+
+            Type[] types = LoadTypes(a);
+            if (types == null)
+            {
+                throw new Exception($"Error getting types from {Path.GetFileName(assemblyFilePath)}");
+            }
+
+            return types;
+        }
+
+        public static Type LoadType(string assemblyFilePath, string className)
+        {
+            var types = LoadTypes(assemblyFilePath);
+
+            var t = types.FirstOrDefault(t => t.FullName == className);
+            if (t == null)
+            {
+                t = types.FirstOrDefault(t => t.Name == className);
+            }
+
+            if (t == null)
+            {
+                throw new Exception($"No {className} found in {Path.GetFileName(assemblyFilePath)}");
+            }
+
+            return t;
+        }
+
+        public static MethodInfo[] LoadMethods(Type t)
+        {
+            MethodInfo[] methods = null;
+            try
+            {
+                methods = t.GetMethods(
+                    BindingFlags.NonPublic |
+                    BindingFlags.Public |
+                    BindingFlags.Static |
+                    BindingFlags.Instance);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return methods.Where(m => !m.IsConstructor && !m.IsSpecialName).ToArray();
+        }
+
+        public static MethodInfo[] LoadMethods(string assemblyFilePath, string className)
+        {
+            var t = LoadType(assemblyFilePath, className);
+            var methods = LoadMethods(t);
+            if (methods == null)
+            {
+                throw new Exception($"Error getting methods from {className}");
+            }
+
+            return methods;
+        }
+
+        public static object[] ConvertParameters(ParameterInfo[] parameterInfos, string[] parameterStrings)
+        {
+            var result = new object[parameterInfos.Length];
+            for (int i = 0; i < parameterInfos.Length; i++)
+            {
+                var paramIsMissing = parameterStrings == null || parameterStrings.Length <= i;
+                if (paramIsMissing)
+                {
+                    if (parameterInfos[i].HasDefaultValue)
+                    {
+                        result[i] = Type.Missing;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Method's parameter #{i + 1} has neither default value, nor value specified. Please use additional -p argument or correct method's signature");
+                    }
+                }
+                else
+                {
+                    result[i] = Convert.ChangeType(parameterStrings[i], parameterInfos[i].ParameterType);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/MethodExecuto/Program.cs
@@ -0,0 +1,235 @@
+﻿// Copyright (c) 2022 George Churochkin. All rights reserved.
+// Licensed under the MIT License (https://opensource.org/licenses/MIT)
+// Full license text: see LICENSE file in the root folder of this repository
+
+namespace rextextau.MethodExecuto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Utility.CommandLine;
+
+    /// <summary>
+    /// Console app to quickly execute a particular method of a particular type in a particular assembly.
+    /// </summary>
+    internal class Program
+    {
+        #region Const
+
+        private const string DLL_SUFFIX = ".dll";
+
+        #endregion
+
+        #region Fields
+
+        [Argument('h', "help", "Show help")]
+        private static bool ShowHelp { get; set; }
+
+        [Argument('d', "directory", "Set working directory")]
+        private static string WorkingDir { get; set; }
+
+        [Argument('a', "assembly", "Specify the assembly name")]
+        private static string AssemblyName { get; set; }
+
+        [Argument('t', "type", "Specity the type name")]
+        private static string TypeName { get; set; }
+
+        [Argument('m', "method", "Specify the method name")]
+        private static string MethodName { get; set; }
+
+        [Argument('p', "parameters", "Specify the parameters' values")]
+        private static string[] ParameterValues { get; set; }
+
+        [Operands]
+        private static List<string> Operands { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("}-=[ Welcome to MethodExecuto! ]=-{");
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine("List of available arguments:");
+            Console.WriteLine("Short Long        Description");
+            Console.WriteLine("----- ----        -----------");
+
+            foreach (var item in Arguments.GetArgumentInfo(typeof(Program)))
+            {
+                Console.WriteLine(
+                    item.ShortName.ToString().PadRight(6) +
+                    item.LongName.PadRight(12) +
+                    item.HelpText);
+            }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("If you don't specify the working directory, current directory will be used");
+            Console.WriteLine("If you don't specify the assembly name, all assemblies within current directory will be listed");
+            Console.WriteLine("If you don't specify the type name, all types within selected assembly will be listed");
+            Console.WriteLine("If you don't specify the method name, all methods within selected type will be listed");
+            Console.WriteLine("If method you've selected to call is not staic, parameterless ctor will be executed (if any)");
+            Console.WriteLine("Multiple parameters' values allowed, i.e. -p \"x\" -p \"2.22\" -p \"3\" will lead to call ('x', 2.22, 3)");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("}-=[ Have a nice day! Regards, rextextau ]=-{");
+        }
+
+        private static string GetAssemblyPath()
+        {
+            return
+                Path.IsPathRooted(AssemblyName) ?
+                AssemblyName :
+                Path.Combine(WorkingDir, AssemblyName);
+        }
+
+        private static void PrintAssemblies()
+        {
+            foreach (string assemblyFilePath in Directory.GetFiles(WorkingDir, "*" + DLL_SUFFIX))
+            {
+                Assembly a = Loader.LoadAssembly(assemblyFilePath);
+                if (a != null)
+                {
+                    Console.WriteLine(Path.GetFileName(assemblyFilePath));
+                }
+            }
+        }
+
+        private static void PrintTypes()
+        {
+            foreach (Type t in Loader.LoadTypes(GetAssemblyPath()))
+            {
+                Console.WriteLine(
+                    (t.IsPublic ? "public " : string.Empty) +
+                    (t.IsAbstract ? "abstract " : string.Empty) +
+                    (t.IsSealed ? "sealed " : string.Empty) +
+                    (t.IsEnum ? "enum " : string.Empty) +
+                    (t.IsClass ? "class " : string.Empty) +
+                    t.Name);
+            }
+        }
+
+        private static void PrintMethods()
+        {
+            foreach (MethodInfo m in Loader.LoadMethods(GetAssemblyPath(), TypeName))
+            {
+                Console.WriteLine(
+                    (m.IsPublic ? "public " : string.Empty) +
+                    (m.IsPrivate ? "private " : string.Empty) +
+                    (m.IsStatic ? "static " : string.Empty) +
+                    (m.IsVirtual ? "virtual " : string.Empty) +
+                    (m.IsAbstract ? "abstract " : string.Empty) +
+                    m.Name);
+            }
+        }
+
+        private static void PrintMethodInvocationResult()
+        {
+            var type = Loader.LoadType(GetAssemblyPath(), TypeName);
+            var methods = Loader.LoadMethods(type);
+            var candidateMethods = methods.Where(m => m.Name == MethodName).ToList();
+            object[] parameters = null;
+            MethodInfo method = null;
+            for (int i = 0; i < candidateMethods.Count; i++)
+            {
+                try
+                {
+                    method = candidateMethods[i];
+                    parameters = Loader.ConvertParameters(method.GetParameters(), ParameterValues);
+                    break;
+                }
+                catch (Exception)
+                {
+                    // if it's not the last method - ignore exception and continue trying
+                    // if last method does not fit - then rethrow the exception, no way:
+                    if (i == candidateMethods.Count - 1)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            object instance = null;
+            if (!method.IsStatic)
+            {
+                // trying to execute ctor with no parameters
+                ConstructorInfo ctor = type.GetConstructor(Type.EmptyTypes);
+                instance = ctor.Invoke(null);
+            }
+
+            var result = method.Invoke(instance, parameters);
+            string resultString = string.Empty;
+            if (result is IConvertible)
+            {
+                resultString = (string)Convert.ChangeType(result, typeof(string));
+            }
+            resultString = string.IsNullOrEmpty(resultString) ? "NULL" : resultString;
+            Console.WriteLine(resultString);
+        }
+
+        private static void Main()
+        {
+            Environment.ExitCode = 0;
+            Arguments.Populate();
+            Debug.WriteLine("Operands: " + string.Join(',', Operands.Skip(1)));
+
+            if (ShowHelp)
+            {
+                PrintHelp();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(WorkingDir))
+            {
+                WorkingDir = Loader.GetAssemblyDir(Assembly.GetExecutingAssembly());
+            }
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(AssemblyName))
+                {
+                    PrintAssemblies();
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(TypeName))
+                {
+                    PrintTypes();
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(MethodName))
+                {
+                    PrintMethods();
+                    return;
+                }
+
+                // adding assemblies resolution from working dir too
+                AppDomain.CurrentDomain.AssemblyResolve += (object sender, ResolveEventArgs args) =>
+                {
+                    string assemblyPath = Path.Combine(WorkingDir, new AssemblyName(args.Name).Name + DLL_SUFFIX);
+                    try
+                    {
+                        return Assembly.LoadFrom(assemblyPath);
+                    }
+                    catch (Exception)
+                    {
+                        return null;
+                    }
+                };
+
+                PrintMethodInvocationResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+                Environment.ExitCode = -1;
+            }
+        }
+
+        #endregion
+    }
+}
